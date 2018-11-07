6e07dd38f46fd90b5e6aaa4e80161f13a9a744cf
Connor Herring <connorherring@Connors-MBP.fc.ca>
2018-11-07 14:02:47 -0500

    committed sass gulp npm files

---
+++ js/main.js
@@ -0,0 +1,6 @@
+TweenMax.from("#Honeycomb2, #Honeycomb1", 1, {scale:0});
+TweenMax.from("#hc_1, #hc_2, #hc_3, #hc_4, #hc_5, #hc_6, #hc_7", 1, {scale:0});
+
+TweenMax.from("#bees1, #bees2", 1, {scale:0});
+
+
